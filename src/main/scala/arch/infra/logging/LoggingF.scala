b675e625f0678db8db43ce75742142c1f0bb8266
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-04-03 14:29:24 +0200

    MINOR

--- src/main/scala/arch/infra/logging/LoggingF.scala
+++ src/main/scala/arch/infra/logging/LoggingF.scala
@@ -4,6 +4,7 @@ import arch.common.Program
 import arch.common.Program.MError
 import org.slf4j.LoggerFactory
 
+// TODO fix why ctx is not being logged
 case class LoggingF[F[_]: MError]() extends LoggingLibrary[F] {
   private val logger = LoggerFactory.getLogger("main-logger")
   override def logDebug(msg: String): Program.Context => F[Unit] =
