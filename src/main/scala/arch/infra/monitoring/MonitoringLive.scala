a1fa833bbf0e754ccb01b139d34980dd3b331fc8
miguelemosreverte <miguelemosreverte@gmail.com>
2022-04-14 23:01:54 +0200

    Moved example to branch 'example'

--- src/main/scala/arch/infra/monitoring/MonitoringLive.scala
+++
@@ -1,8 +0,0 @@
-package arch.infra.monitoring
-
-import arch.common.ProgramLive
-
-object MonitoringLive {
-  object MonitoringApp extends MonitoringF[ProgramLive.App]
-  object MonitoringTest extends MonitoringF[ProgramLive.Test]
-}
